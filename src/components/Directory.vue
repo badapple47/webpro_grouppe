841b63a33ecf5a40953122d0568e07403b8509d0
Pathompong Chaisri <kiraboshi47@hotmail.com>
2018-05-11 12:19:14 +0700

    prepare for present

--- src/components/Directory.vue
+++ src/components/Directory.vue
@@ -8,7 +8,7 @@
       <div class="row">
         <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6" >
           <h3 id="alumnia-detail">สมาชิกทั้งหมด  {{Users.length}} คน</h3>
-          <h3 id="alumnia-detail" style="color:#ffcc00; margin-top:0%;">อัพเดทล่าสุด 5/5/2018</h3>
+          <!-- <h3 id="alumnia-detail" style="color:#ffcc00; margin-top:0%;">อัพเดทล่าสุด 5/5/2018</h3> -->
         </div>
         <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6" id="search">
           <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
